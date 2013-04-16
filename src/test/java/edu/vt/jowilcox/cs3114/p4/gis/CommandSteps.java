1e51745a93648e1e8d5322eaae32f3b1e7d79f60
Jonavon Wilcox <jonavon@vt.edu>
2013-04-16 11:56:14 -0400

    Added test stubs for command.

---
+++ src/test/java/edu/vt/jowilcox/cs3114/p4/gis/CommandSteps.java
@@ -0,0 +1,67 @@
+package edu.vt.jowilcox.cs3114.p4.gis;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ * @author jonavon
+ * 
+ */
+public class CommandSteps {
+	
+	@Given("^a database file$")
+	public void a_database_file() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Given("^the database file exists$")
+	public void the_database_file_exists() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@When("^the system is run$")
+	public void the_system_is_run() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Then("^the existing file should be truncated.$")
+	public void the_existing_file_should_be_truncated() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Given("^a command file$")
+	public void a_command_file() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Given("^the command file does not exists$")
+	public void the_command_file_does_not_exists() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Then("^an error message should be written to the console:$")
+	public void an_error_message_should_be_written_to_the_console(String arg1) throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Given("^a log file$")
+	public void a_log_file() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Given("^the log file exists$")
+	public void the_log_file_exists() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+}
