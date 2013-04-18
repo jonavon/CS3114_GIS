62fd13f6e538122ebe8db681b5afaeaffd51b414
Jonavon Wilcox <jonavon@vt.edu>
2013-04-18 11:42:48 -0400

    Comments to logfile object.

--- src/main/java/edu/vt/jowilcox/cs3114/p4/gis/GISLogFile.java
+++ src/main/java/edu/vt/jowilcox/cs3114/p4/gis/GISLogFile.java
@@ -8,18 +8,40 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 
 /**
+ * Represents the logfile.
+ * 
  * @author "Jonavon Wilcox <jowilcox@vt.edu>"
- *
  */
 public class GISLogFile extends AbstractGISFile {
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param filename
+	 *          absolute path of log file.
+	 * @throws FileNotFoundException
+	 */
 	public GISLogFile(String filename) throws FileNotFoundException {
-		this.file = new RandomAccessFile(filename, AbstractGISFile.GIS_FILE_READWRITE);
-  }
+		this.file = new RandomAccessFile(filename,
+		    AbstractGISFile.GIS_FILE_READWRITE);
+	}
+
+	/**
+	 * Write a string as is to the log file.
+	 * 
+	 * @param data
+	 * @throws IOException
+	 */
 	public void log(String data) throws IOException {
 		this.file.write(data.getBytes());
 	}
+
+	/**
+	 * Remove the contents of the file.
+	 * 
+	 * @throws IOException
+	 */
 	public void truncate() throws IOException {
 		this.file.setLength(0);
-  }
+	}
 }
