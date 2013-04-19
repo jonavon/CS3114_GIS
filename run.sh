1485e9bd0b91815e77a3a2958591a7774adb17de
Jonavon Wilcox <jonavon@vt.edu>
2013-04-19 02:31:05 -0400

    Add unix time command to determine how long runs take.

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ mvn clean package \
 	&& ln -vf ./target/test-classes/files/VA_Monterey.txt ./VA_Monterey.txt \
 	&& ln -vf ./target/test-classes/files/NM_All.txt ./NM_All.txt \
 	&& ln -vf ./target/test-classes/files/CO_All.txt ./CO_All.txt \
-	&& ./target/GIS.jar /tmp/db.txt ./target/test-classes/files/command/Script08.txt /tmp/log.txt;
+	&& time ./target/GIS.jar /tmp/db.txt ./target/test-classes/files/command/Script08.txt /tmp/log.txt;
