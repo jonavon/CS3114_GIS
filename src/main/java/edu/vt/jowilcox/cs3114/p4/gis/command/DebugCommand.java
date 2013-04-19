223a4b0d186984a6cf346a0251342e94b0f0e2f8
Jonavon Wilcox <jonavon@vt.edu>
2013-04-19 04:08:55 -0400

    Started debug command.

--- src/main/java/edu/vt/jowilcox/cs3114/p4/gis/command/DebugCommand.java
+++ src/main/java/edu/vt/jowilcox/cs3114/p4/gis/command/DebugCommand.java
@@ -1,30 +1,51 @@
 package edu.vt.jowilcox.cs3114.p4.gis.command;
 
+import java.io.IOException;
+
 /**
  * Class DebugCommand
  */
 public class DebugCommand extends AbstractCommand {
+	private DebugCommand.TYPE debug;
 
 	//
 	// Fields
 	//
+	enum TYPE {
+		quad, hash, pool
+	}
 
 	//
 	// Constructors
 	//
 
-	// TODO
 	public DebugCommand(String args) {
 		this(args.split("\\s"));
 	}
 
-	// TODO
 	public DebugCommand(String... args) {
+		this.debug = DebugCommand.TYPE.valueOf(args[0]);
 	}
 
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-		System.out.println("Debug command executed.");
+		try {
+			switch (this.debug) {
+				case quad:
+					this.logfile.log(this.database.getCoordIndex().print(false));
+				break;
+				case hash:
+					this.logfile.log(this.database.getNameIndex().debug());
+				break;
+				// TODO Add a buffer pool debug
+				default:
+					this.logfile.log("Invalid command");
+				break;
+			}
+		}
+		catch (IOException e) {
+			System.err.println("Unable to print debug data do to error");
+			e.printStackTrace();
+		}
 	}
 }
