a99f02e924eb5ddb64adea1c9213928f501650ad
Jonavon Wilcox <jonavon@vt.edu>
2013-04-20 13:28:05 -0400

    Lighter tests.

--- run.sh
+++ run.sh
@@ -5,4 +5,7 @@ mvn clean package \
 	&& ln -vf ./target/test-classes/files/VA_Monterey.txt ./VA_Monterey.txt \
 	&& ln -vf ./target/test-classes/files/NM_All.txt ./NM_All.txt \
 	&& ln -vf ./target/test-classes/files/CO_All.txt ./CO_All.txt \
-	&& time ./target/GIS.jar /tmp/db.txt ./target/test-classes/files/command/Script08.txt /tmp/log.txt;
+	&& ln -vf ./target/test-classes/files/VA_Montgomery.txt ./ \
+	&& ln -vf ./target/test-classes/files/VA_Highland.txt ./ \
+	&& ln -vf ./target/test-classes/files/VA_Bath.txt ./ \
+	&& time ./target/GIS.jar /tmp/db.txt ./target/test-classes/files/command/Script03.txt /tmp/log.txt;
