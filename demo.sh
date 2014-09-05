60ca27080e62c2d3756385f03cee08042589942c
Jonavon Wilcox <jowilcox@vt.edu>
2014-09-05 09:03:00 -0400

    Updated commands to reflect uncustomized build.

--- demo.sh
+++ demo.sh
@@ -4,13 +4,16 @@
 wget 'http://courses.cs.vt.edu/~cs3114/Spring13/barnette/assignments/p4/GIS-DemoFiles.zip';
 
 # Extract jar file.
-jar -xvf jowilcox.Project4.2.jar;
+# Not necessary in the source folder
+# Runing the below git command will create a jar archiue
+# $> git archive HEAD --format=zip -o jowilcox.Project4.2.jar
+# jar -xvf jowilcox.Project4.2.jar;
 
 # Make compile directory.
 mkdir bin;
 
 # Compile source files.
-javac -d ./bin `\tree -fli | grep .java`;
+find ./src/main/java -name "*.java" | xargs javac -d ./bin
 
 # Create executable jar file.
 cd bin/;
@@ -24,13 +27,13 @@ unzip  GIS-DemoFiles.zip;
 cd GIS-DemoFiles/;
 
 # Run scripts.
-time ../GIS {jowilcoxDB,DemoScript,jowilcoxLog}01.txt;
-time ../GIS {jowilcoxDB,DemoScript,jowilcoxLog}02.txt;
-time ../GIS {jowilcoxDB,DemoScript,jowilcoxLog}03.txt;
-time ../GIS {jowilcoxDB,DemoScript,jowilcoxLog}04.txt;
-time ../GIS {jowilcoxDB,DemoScript,jowilcoxLog}05.txt;
-time ../GIS {jowilcoxDB,DemoScript,jowilcoxLog}06.txt;
-time ../GIS {jowilcoxDB,DemoScript,jowilcoxLog}07.txt;
+time ../GIS {demoDB,DemoScript,jowilcoxLog}01.txt;
+time ../GIS {demoDB,DemoScript,jowilcoxLog}02.txt;
+time ../GIS {demoDB,DemoScript,jowilcoxLog}03.txt;
+time ../GIS {demoDB,DemoScript,jowilcoxLog}04.txt;
+time ../GIS {demoDB,DemoScript,jowilcoxLog}05.txt;
+time ../GIS {demoDB,DemoScript,jowilcoxLog}06.txt;
+time ../GIS {demoDB,DemoScript,jowilcoxLog}07.txt;
 
  Open Scripts.
 \gvim -m --servername "GIS-DEMO" --remote-wait-silent +":vsp jowilcoxLog01.txt" DemoLog01.txt;
